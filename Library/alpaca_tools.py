0ef9fb55b97b84656936828fe2f12343f0047f0c
Niko <nikojlarin22@gmail.com>
2025-12-30 10:01:38 -0500

    Update alpaca_tools.py

--- Library/alpaca_tools.py
+++ Library/alpaca_tools.py
@@ -39,5 +39,41 @@ def pctDailyChange(sym):
     pctChange = round(((closePrice - openPrice) / openPrice), 4) * 100
 
     return pctChange
+
+def get_open_positions(): # return all open positions
+    response = requests.get(f'{BASE_URL}/positions', headers=headers()) # calls headers
+    response = response.json()
     
+    return response['positions'] # all open positions
+
+def is_market_open():
+    response = requests.get(f'{BASE_URL}/clock', headers=headers()) # calls headers
+    response = response.json()
+
+    return response['is_open'] #returns bool if mkt open/closed
+
+def option(ticker, oCode): #needs symbol and option code
+    response = requests.get(f"https://data.alpaca.markets/v1beta1/options/snapshots/{ticker}?feed=opra&limit=100&root_symbol={oCode}", headers=headers()) # gets live option based on oCode
+    response = response.json()
+
+    snap = response["snapshots"][oCode]
+
+    greeks = response['greeks']
+    impliedVolatility = snap['impliedVolatility'] 
+    price = snap['latestQuote']['ap']
+
+    return (greeks, impliedVolatility, price) # returnts a tuple
+
+def options_chain():
+    #implement a simple filter to only get the most recent x strikes
+    response = requests.get(f"https://data.alpaca.markets/v1beta1/options/snapshots/{ticker}")
+    response = response.json()
+
+    return response['snapshots'] # returns huge chain
+
+def position_history():
+    pass
+
+def chart():
+    pass
 
