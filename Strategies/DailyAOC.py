1c722a6361e1c56d053f192f4e9816fb9a676b1f
Niko <nikojlarin22@gmail.com>
2026-01-01 15:33:45 -0500

    Enhance aoc_strategy for multiple tickers
    Refactor aoc_strategy to handle multiple tickers and improve order execution logic.

--- Strategies/DailyAOC.py
+++ Strategies/DailyAOC.py
@@ -4,79 +4,80 @@ This strategy is based on my own Average Open-Close Change indicator which takes
 A credit spread is executed when an equities price goes above/below the AOC. This strategy can be classified as a mean reversion strategy and is executed currently using only 0-DTE options.
 
 FUTURE UPDATES:
-1. Allow for a list of [ticker] so we can use this on more than one name at a time.
-2. Dynamic allocation: 'I want to use 5% of my portfolio for this strategy'.
-3. Instead of executing at the market price, set a limit order at a more favorable fill.  
-4. Bug fixes.
+1. Dynamic allocation: 'I want to use 5% of my portfolio for this strategy'.
+2. Instead of executing at the market price, set a limit order at a more favorable fill.  
+3. Bug fixes.
 '''
 
 import requests
 import time
 from datetime import date
 from dateutil.relativedelta import relativedelta
-from alpaca_tools import headers, open_stock_price, current_stock_price, aoc, BASE_URL
+from alpaca_tools import headers, open_stock_price, current_stock_price, aoc
 
 def aoc_strategy(ticker):
-        while True:    
-            hour = time.localtime() # current time
-            while hour[3] > 9 or hour[3] < 16:
-                today_aoc = aoc(ticker)
-                stock_price = current_stock_price(ticker)
-                
-                today = str(date.today())
-                today = today.replace('-', '')
+            for t in ticker:
+                while True:    
+                    hour = time.localtime()
+                    while hour[3] > 9 or hour[3] < 16:
+                        today_aoc = aoc(t)
+                        stock_price = current_stock_price(t)
+                        
+                        today = str(date.today())
+                        today = today.replace('-', '')
 
-                today_open = open_stock_price(ticker)
-                highbar = today_open + today_open * today_aoc
-                lowbar =  today_open - today_open * today_aoc
+                        today_open = open_stock_price(t)
+                        highbar = today_open + today_open * today_aoc
+                        lowbar =  today_open - today_open * today_aoc
 
-                if stock_price > highbar: # if price is higher than top AOC
-                    otype = 'C'
-                    strike = round(stock_price, 0)
-                    long_leg = f'{ticker}{today[2:]}{otype}00{int(strike + 1)}000'
-                
-                elif stock_price < lowbar: #if price is lower than bottom AOC
-                    otype = 'P'
-                    strike = round(stock_price, 0)
-                    long_leg = f'{ticker}{today[2:]}{otype}00{int(strike - 1)}000'
-                
-                else:
-                     print(f'Highbar:{highbar} Lowbar: {lowbar}')
-                     print("Waiting")
-                     time.sleep(60)
-                     continue
-                
-                short_leg = f'{ticker}{today[2:]}{otype}00{int(strike)}000'
-                
-                url = f'{BASE_URL}/orders'
 
-                payload = { #creates the debit spread
-                    "type": "market",
-                    "time_in_force": "day",
-                    "legs": [
-                        {
-                            "side": "buy",
-                            "symbol": long_leg,
-                            "ratio_qty": "1"
-                        },
-                        {
-                            "side": "sell",
-                            "symbol": short_leg,
-                            "ratio_qty": "1"
+                        if stock_price > highbar:
+                            otype = 'C'
+                            strike = round(stock_price, 0)
+                            long_leg = f'{ticker}{today[2:]}{otype}00{int(strike + 1)}000'
+                        
+                        elif stock_price < lowbar:
+                            otype = 'P'
+                            strike = round(stock_price, 0)
+                            long_leg = f'{t}{today[2:]}{otype}00{int(strike - 1)}000'
+                        
+                        else:
+                            print(f'Highbar:{highbar} Lowbar: {lowbar}')
+                            print("Waiting")
+                            time.sleep(60)
+                            continue
+                        
+                        short_leg = f'{t}{today[2:]}{otype}00{int(strike)}000'
+                        
+                        url = "https://paper-api.alpaca.markets/v2/orders"
+
+                        payload = {
+                            "type": "market",
+                            "time_in_force": "day",
+                            "legs": [
+                                {
+                                    "side": "buy",
+                                    "symbol": long_leg,
+                                    "ratio_qty": "1"
+                                },
+                                {
+                                    "side": "sell",
+                                    "symbol": short_leg,
+                                    "ratio_qty": "1"
+                                }
+                            ],
+                            "order_class": "mleg",
+                            "qty": "1"
                         }
-                    ],
-                    "order_class": "mleg",
-                    "qty": "1"
-                }
 
-                response = requests.post(url, json=payload, headers=headers())
+                        response = requests.post(url, json=payload, headers=headers())
 
-                print(response.text)
-                break
-            break # ends the loop once a trade is executed, only one trade a day.
-            
-print(aoc('SPY')) # just prints the AOC that was calculated
-aoc_strategy('SPY') # runs strategy
+                        print(response.text)
+                        break
+                    break
 
 
-
+ticker = ['SPY', 'QQQ']
+for t in ticker:
+    print(aoc(t))
+aoc_strategy(ticker)
